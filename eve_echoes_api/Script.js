4331b58054aa69bd8d1b4016ad0ee4a1c6573c30
Asnise_Git <55057145+asnise@users.noreply.github.com>
2021-06-20 11:51:58 +0700

    Add files via upload

--- eve_echoes_api/Script.js
+++ eve_echoes_api/Script.js
@@ -1,39 +1,62 @@
-var url = "https://api.eve-echoes-market.com/market-stats/stats.csv";
-var data;
-var data_ajax;
-var rows;
-var res;
-var data_call = [];
-var xhr = new XMLHttpRequest();
-xhr.open("GET", url);
-
-var obj = {};
-
-
-xhr.onreadystatechange = function () {
-   if (xhr.readyState === 4) {
-	   data = xhr.responseText;
-
-      rows = data.split("\r\n");
-      for (var i = 0; i < rows.length; i++) {
-         var cells = rows[i].split(",");
-         if (cells.length > 1) {
-            var customer = {};
-             customer[cells[1]] = {
-               id: cells[0],
-               name: cells[1],
-               time: cells[2],
-               sell: cells[3],
-               buy: cells[4],
-               lowest_sell: cells[5],
-               highest_buy: cells[6],
-           }
-           data_call.push(customer);
+var request = new XMLHttpRequest();
+request.open('GET', 'https://api.eve-echoes-market.com/market-stats/stats.csv', true);
+var data_call= {};
+request.send();
+request.onload = function(){ 
+   if (request.status >= 200 && request.status < 400) {
+   var data = request.responseText;
+   var rows = data.split("\r\n");
+   for (var i = 0; i < rows.length; i++) {
+      var cells = rows[i].split(",");
+      if (cells.length > 1) {
+         data_call[cells[1].replace(" ", "").replace(" ", "").replace(" ", "").replace(" ", "").replace(" ", "").replace(" ", "").replace(" ", "").toLowerCase()] = {
+            id: cells[0],
+            name: cells[1],
+            time: cells[2],
+            sell: cells[3],
+            buy: cells[4],
+            lowest_sell: cells[5],
+            highest_buy: cells[6]
          }
+         
       }
-	   document.getElementById("dvCSV").innerHTML = JSON.stringify(data_call);
-      obj = JSON.stringify(data_call);
-   }};
+   }
+   console.log(data_call);
+   document.getElementById("dvCSV").innerHTML = JSON.stringify(data_call);
+ }
+};
 
-xhr.send();
+/*$(document).ready(function(){
+   $.ajax({
+     url: 'https://api.eve-echoes-market.com/market-stats/stats.csv' ,
+     format: '',
+     dataType: "",
+     success(data){
+        var rows;
+        rows = data.split("\r\n");
+        for (var i = 0; i < rows.length; i++) {
+           var cells = rows[i].split(",");
+           if (cells.length > 1) {
+              var data_call = {};
+              data_call[cells[1].replace(" ", "").replace(" ", "").replace(" ", "").replace(" ", "").replace(" ", "").replace(" ", "").replace(" ", "")] = {
+                 id: cells[0],
+                 name: cells[1],
+                 time: cells[2],
+                 sell: cells[3],
+                 buy: cells[4],
+                 lowest_sell: cells[5],
+                 highest_buy: cells[6]
+              }
+              data_call.push(data_call);
+           }
+        }
+        obj = JSON.stringify(data_call);
+        document.getElementById("dvCSV").innerHTML = obj;
+     }
+   });
+});*/
+
+
+
+
 
