01417ae24c864ddbf13c9a96924cec3ad5219e55
Asnise_Git <55057145+asnise@users.noreply.github.com>
2021-07-24 00:12:14 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,55 +6,66 @@ var request = new XMLHttpRequest();
 request.open('GET', 'https://api.countapi.xyz/hit/asnise.github.io/main', true);
 request.send();
 request.onload = function () {
-    if (request.status >= 200 && request.status < 400) {
+	if (request.status >= 200 && request.status < 400) {
 		data = JSON.parse(request.responseText);
 		document.getElementById("visits").innerHTML = "ตอนนี้มีคนเข้ามาดูแล้ว : " + data.value + " ครั้ง";
 	}
 }
 
-function cansel_win(idtag ,name,content,img_1,img_2,img_3,download)
-{
+function img_win(idtag,scr) {
 	var win_alert = document.getElementById(idtag);
-	if (win_alert.style.display === "block")
-	{
+	var img = document.getElementById("imgpre");
+	if (win_alert.style.display === "block") {
 		win_alert.style.display = "none";
 	}
-	else
-	{
+	else {
 		win_alert.style.display = "block";
+		img.src = scr;
 	}
-		document.getElementById("img_1").src = img_1;
-		image_[0] = img_1;
-		image_[1] = img_2;
-		image_[2] = img_3;
-		document.getElementById("title_name").innerHTML = name;
-		document.getElementById("content_mod").innerHTML = content;
-		document.getElementById("download_bt").href = download;
-		
-	
 };
 
-function slider_bt(trun)
-{
-	if(trun == "next")
-	{
+function cansel_win(idtag) {
+	var win_alert = document.getElementById(idtag);
+	if (win_alert.style.display === "block") {
+		win_alert.style.display = "none";
+	}
+	else {
+		win_alert.style.display = "block";
+	}
+};
+
+
+function slid_win(idtag, more) {
+	var win_alert = document.getElementById(idtag);
+	var mores = document.getElementById(more);
+
+	if (win_alert.style.display === "block") {
+		win_alert.style.display = "none";
+		mores.innerHTML = "อ่านเพิ่ม";
+	}
+	else {
+		win_alert.style.display = "block";
+		mores.innerHTML = "แสดงน้อยลง";
+	}
+
+};
+
+function slider_bt(trun) {
+	if (trun == "next") {
 		num += 1;
 	}
-	
-	if(trun == "back")
-	{
+
+	if (trun == "back") {
 		num -= 1;
 	}
-	
-	if(num < 0)
-	{
+
+	if (num < 0) {
 		num = 2;
 	}
-	
-	if(num > 2)
-	{
+
+	if (num > 2) {
 		num = 0;
 	}
-	
+
 	document.getElementById("img_1").src = image_[num];
 }
